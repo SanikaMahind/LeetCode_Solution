53b2140afb9e99ff611888e8201a602e5f745ee4
sanika mahind <mahindsanika03@gmail.com>
2026-01-26 21:13:13 +0530

    Create RemoveDuplicates.cpp

---
+++ RemoveDuplicates.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 1, 2, 2, 3};
+
+    int k = sol.removeDuplicates(nums);
+
+    cout << "New length: " << k << endl;
+    cout << "Array after removing duplicates: ";
+
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
