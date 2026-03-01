4989b6ffb24286d1ade8daf5878c32bb037167ec
sanika mahind <mahindsanika03@gmail.com>
2026-03-01 23:28:39 +0530

    Create InputArrayIsSorted.cpp

---
+++ InputArrayIsSorted.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+        
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            
+            if (sum == target) {
+                return {left + 1, right + 1};  
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        
+        return {};
+    }
+};
+
+int main() {
+    Solution obj;
+    
+    vector<int> numbers = {2, 7, 11, 15};
+    int target = 9;
+    
+    vector<int> result = obj.twoSum(numbers, target);
+    
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << " " << result[1] << endl;
+    } else {
+        cout << "No solution found" << endl;
+    }
+    
+    return 0;
+}
