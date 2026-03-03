88b721b00373ff0925acc396af07fda688a430b2
sanika mahind <mahindsanika03@gmail.com>
2026-03-03 22:59:22 +0530

    Create RotateImage.cpp

---
+++ RotateImage.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+      
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<vector<int>> matrix = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    obj.rotate(matrix);
+
+    cout << "Rotated Matrix:\n";
+    for (auto row : matrix) {
+        for (auto val : row)
+            cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
