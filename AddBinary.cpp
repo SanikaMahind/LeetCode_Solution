adaf401af8d5ba4697df08d9347f25f0179d93d6
sanika mahind <mahindsanika03@gmail.com>
2026-03-20 22:23:13 +0530

    Create AddBinary.cpp

---
+++ AddBinary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string addBinary(string a, string b) {
+    string result = "";
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    int carry = 0;
+
+  
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+
+        if (i >= 0) {
+            sum += a[i] - '0';
+            i--;
+        }
+
+        if (j >= 0) {
+            sum += b[j] - '0';
+            j--;
+        }
+
+        
+        result = char((sum % 2) + '0') + result;
+
+        
+        carry = sum / 2;
+    }
+
+    return result;
+}
+
+int main() {
+    string a = "1010";
+    string b = "1011";
+
+    cout << addBinary(a, b);
+
+    return 0;
+}
