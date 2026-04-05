7c15b68cbbe9b42f1818ff93e9d783520e8751d5
sanika mahind <mahindsanika03@gmail.com>
2026-04-05 23:00:27 +0530

    Create RemoveN-nodeFromEnd.cpp

---
+++ RemoveN-nodeFromEnd.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
+        }
+        while (fast != NULL) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        ListNode* toDelete = slow->next;
+        slow->next = slow->next->next;
+        delete toDelete;
+
+        return dummy->next;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    int n = 2;
+
+    Solution obj;
+    head = obj.removeNthFromEnd(head, n);
+
+    cout << "After Removing " << n << "th Node from End: ";
+    printList(head);
+
+    return 0;
+}
