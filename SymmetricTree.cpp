0f8471487ba0f564c421a085eaa2730067271d91
sanika mahind <mahindsanika03@gmail.com>
2026-04-21 23:48:06 +0530

    Create SymmetricTree.cpp

---
+++ SymmetricTree.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+bool isMirror(TreeNode* t1, TreeNode* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return true;
+
+    if (t1 == NULL || t2 == NULL)
+        return false;
+
+    return (t1->val == t2->val) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+
+bool isSymmetric(TreeNode* root) {
+    return isMirror(root, root);
+}
+
+
+int main() {
+    
+
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
+
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(3);
+
+    if (isSymmetric(root))
+        cout << "Tree is Symmetric" << endl;
+    else
+        cout << "Tree is NOT Symmetric" << endl;
+
+    return 0;
+}
