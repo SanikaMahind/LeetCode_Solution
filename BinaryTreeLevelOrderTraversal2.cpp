798b8c5bb132164782cb0bfa185b4e6537d7bc16
sanika mahind <mahindsanika03@gmail.com>
2026-04-26 23:11:01 +0530

    Create BinaryTreeLevelOrderTraversal2.cpp

---
+++ BinaryTreeLevelOrderTraversal2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if (node->left)  q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            result.push_back(level);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution sol;
+    vector<vector<int>> res = sol.levelOrderBottom(root);
+
+    for (auto level : res) {
+        for (int val : level) cout << val << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
