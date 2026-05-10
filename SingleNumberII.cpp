e180f356fb684dd07c4354b065a5648eda549dc8
sanika mahind <mahindsanika03@gmail.com>
2026-05-10 23:40:13 +0530

    Create SingleNumberII.cpp

---
+++ SingleNumberII.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+
+        return ones;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {2, 2, 3, 2};
+
+    cout << "Single Number is: "
+         << obj.singleNumber(nums);
+
+    return 0;
+}
