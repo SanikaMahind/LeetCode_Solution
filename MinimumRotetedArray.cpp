8ef2d7a047728c931cdc5f2d85bf1c3ad0e1b491
sanika mahind <mahindsanika03@gmail.com>
2026-05-17 22:05:37 +0530

    Create MinimumRotetedArray.cpp

---
+++ MinimumRotetedArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+
+        
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+           
+            else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {3,4,5,1,2};
+
+    cout << "Minimum element = "
+         << obj.findMin(nums);
+
+    return 0;
+}
