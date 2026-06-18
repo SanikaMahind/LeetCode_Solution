bc3c6fe71eb92d5ef64721734ad34dc11fcede3c
sanika mahind <mahindsanika03@gmail.com>
2026-06-18 23:46:50 +0530

    Create OrderlyQueue.cpp

---
+++ OrderlyQueue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            string ans = s;
+
+            for (int i = 1; i < s.size(); i++) {
+                string rotated = s.substr(i) + s.substr(0, i);
+                ans = min(ans, rotated);
+            }
+
+            return ans;
+        }
+
+        sort(s.begin(), s.end());
+        return s;
+    }
+};
