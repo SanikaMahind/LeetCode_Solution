66fd168629dfdeeb190d0447176055a2bb6dec37
sanika mahind <mahindsanika03@gmail.com>
2026-07-10 22:51:20 +0530

    Create MinimumScoreTriangulation.cpp

---
+++ MinimumScoreTriangulation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
+
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j],
+                                   dp[i][k] + dp[k][j] +
+                                   values[i] * values[k] * values[j]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> values(n);
+    for (int i = 0; i < n; i++) {
+        cin >> values[i];
+    }
+
+    Solution obj;
+    cout << obj.minScoreTriangulation(values) << endl;
+
+    return 0;
+}
